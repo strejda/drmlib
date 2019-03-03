f6bd75392da03cbcec36a0166efd870a072426bd
Michal Meloun <strejda@users.noreply.github.com>
2019-03-03 09:46:46 +0100

    LKPI: Cleeanup

--- sys/arm/dev/drmlib/gpl/include/linux/compiler.h
+++ sys/arm/dev/drmlib/gpl/include/linux/compiler.h
@@ -3,8 +3,14 @@
 
 #include_next <linux/compiler.h>
 
-#include <sys/syslog.h>
 #include <linux/types.h>
+#include <linux/bitops.h>
+#include <linux/math64.h>
+
+#include <sys/syslog.h>
+#include <sys/types.h>
+
+#define	idr_init_base(idr,base)	idr_init(idr)
 
 #ifndef PRINT_UNIMPLEMENTED
 #define PRINT_UNIMPLEMENTED 1
@@ -37,11 +43,6 @@
 #define	WARN_NOT()	UNIMPLEMENTED_ONCE()
 #define	DODGY()		DODGY_ONCE()
 
-#define	unreachable()	__unreachable()
-
-// BSDFIXME! (everything to end of this file)
-
-#include <linux/bitops.h>
 
 // XXX: Move to better place?
 static inline void *
@@ -71,27 +72,6 @@ memset_p(void **p, void *v, size_t n)
 		return memset32((uint32_t *)p, (uintptr_t)v, n);
 	else
 		return memset64((uint64_t *)p, (uintptr_t)v, n);
-}
-
-#include <sys/types.h>
-#include <linux/math64.h>
-
-#define	idr_init_base(idr,base)	idr_init(idr)
-// The old style
-#define	atomic_fetch_inc(v)	(atomic_inc_return(v) - 1)
-
-static inline int
-pfn_valid(unsigned long pfn)
-{
-	return 1;
-}
-
-struct linux_kmem_cache;
-static inline int
-kmem_cache_shrink(struct linux_kmem_cache *c)
-{
-	UNIMPLEMENTED();
-	return 0;
 }
 
 static inline uint64_t mul_u64_u32_div(uint64_t a, uint32_t mul, uint32_t divisor)
